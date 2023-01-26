e79740086399a8ea644f7fe4cae92389f8d3f9d4
perama-v <83961755+perama-v@users.noreply.github.com>
2023-01-27 08:32:29 +1100

    add message on migration failure panic

--- glados-monitor/src/main.rs
+++ glados-monitor/src/main.rs
@@ -38,7 +38,7 @@ async fn main() -> Result<(), DbErr> {
 
     if cli.migrate || cli.database_url == "sqlite::memory:" {
         info!("running database migrations");
-        Migrator::up(&conn, None).await.unwrap();
+        Migrator::up(&conn, None).await.expect("Database migration failed");
     }
 
     let task_handle = match &cli.command {
