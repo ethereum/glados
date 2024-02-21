83026796c5774ccca22a37963c7079807f62a7d2
mrferris <mrferris@buffalo.edu>
2024-02-20 19:27:19 -0500

    feat: adds fourfours strategy to glados-audit

--- glados-audit/src/cli.rs
+++ glados-audit/src/cli.rs
@@ -9,6 +9,13 @@ const DEFAULT_STATS_PERIOD: &str = "300";
 pub struct Args {
     #[arg(short, long, default_value = DEFAULT_DB_URL)]
     pub database_url: String,
+    #[arg(
+        short,
+        long,
+        default_value = "",
+        help = "web3 api provider url, eg https://mainnet.infura.io/v3/..."
+    )]
+    pub provider_url: String,
     #[arg(short, long, default_value = "4", help = "number of auditing threads")]
     pub concurrency: u8,
     #[arg(short, long, action(ArgAction::Append), value_enum, default_value = None, help = "Specific strategy to use. Default is to use all available strategies. May be passed multiple times for multiple strategies (--strategy latest --strategy random). Duplicates are permitted (--strategy random --strategy random).")]
@@ -41,9 +48,15 @@ pub struct Args {
         help = "relative weight of the 'random' strategy"
     )]
     pub random_strategy_weight: u8,
+    #[arg(
+        long,
+        default_value = "1",
+        help = "relative weight of the 'four_fours' strategy"
+    )]
+    pub four_fours_strategy_weight: u8,
     #[arg(long, default_value = DEFAULT_STATS_PERIOD, help = "stats recording period (seconds)")]
     pub stats_recording_period: u64,
-    #[arg(short, long, action(ArgAction::Append))]
+    #[arg(long, action(ArgAction::Append))]
     pub portal_client: Vec<String>,
     #[command(subcommand)]
     pub subcommand: Option<Command>,
@@ -63,11 +76,13 @@ impl Default for Args {
     fn default() -> Self {
         Self {
             database_url: DEFAULT_DB_URL.to_string(),
+            provider_url: "".to_string(),
             concurrency: 4,
             latest_strategy_weight: 1,
             failed_strategy_weight: 1,
             oldest_strategy_weight: 1,
             random_strategy_weight: 1,
+            four_fours_strategy_weight: 1,
             strategy: None,
             portal_client: vec!["ipc:////tmp/trin-jsonrpc.ipc".to_owned()],
             subcommand: None,
@@ -157,6 +172,26 @@ mod test {
                 SelectionStrategy::Latest,
                 SelectionStrategy::Random,
             ]),
+            portal_client: vec![PORTAL_CLIENT_STRING.to_owned()],
+            ..Default::default()
+        };
+        assert_eq!(result, expected);
+    }
+
+    /// Tests that the provider_url is passed through properly.
+    #[test]
+    fn test_provider_url() {
+        const PORTAL_CLIENT_STRING: &str = "ipc:////path/to/ipc";
+        const PROVIDER_URL: &str = "https://example.io/key";
+        let result = Args::parse_from([
+            "test",
+            "--provider-url",
+            PROVIDER_URL,
+            "--portal-client",
+            PORTAL_CLIENT_STRING,
+        ]);
+        let expected = Args {
+            provider_url: PROVIDER_URL.to_string(),
             portal_client: vec![PORTAL_CLIENT_STRING.to_owned()],
             ..Default::default()
         };
