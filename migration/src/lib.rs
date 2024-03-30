0b2b573b9d7a0ea097ce7a5919f8778be54f66de
mrferris <mrferris@buffalo.edu>
2024-03-30 17:15:56 -0400

    fix: reverts migration ordering that should not have changed

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -18,7 +18,6 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20230508_111707_create_census_tables::Migration),
             Box::new(m20230511_104804_create_node::Migration),
             Box::new(m20230511_104811_create_record::Migration),
             Box::new(m20230511_104814_create_content::Migration),
@@ -26,6 +25,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20230511_104830_create_content_audit::Migration),
             Box::new(m20230511_104838_create_execution_metadata::Migration),
             Box::new(m20230511_104937_create_key_value::Migration),
+            Box::new(m20230508_111707_create_census_tables::Migration),
             Box::new(m20231107_004843_create_audit_stats::Migration),
             Box::new(m20240213_190221_add_fourfours_stats::Migration),
             Box::new(m20240322_205213_add_content_audit_index::Migration),
