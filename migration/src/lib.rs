f46d664e915e8e6911278b245ade278cc38968d6
Jason Carver <ut96caarrs@snkmail.com>
2025-01-29 21:17:10 -0800

    WIP: migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -17,6 +17,7 @@ mod m20240814_121507_census_subnetwork;
 mod m20240919_121611_census_subnetwork_index;
 mod m20241010_151313_audit_stats_performance;
 mod m20241206_154045_add_beacon_and_state_audit_stats;
+mod m20250130_042751_create_audit_transfer_failures;
 
 pub struct Migrator;
 
@@ -24,6 +25,7 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
+            Box::new(m20230508_111707_create_census_tables::Migration),
             Box::new(m20230511_104804_create_node::Migration),
             Box::new(m20230511_104811_create_record::Migration),
             Box::new(m20230511_104814_create_content::Migration),
@@ -31,7 +33,6 @@ impl MigratorTrait for Migrator {
             Box::new(m20230511_104830_create_content_audit::Migration),
             Box::new(m20230511_104838_create_execution_metadata::Migration),
             Box::new(m20230511_104937_create_key_value::Migration),
-            Box::new(m20230508_111707_create_census_tables::Migration),
             Box::new(m20231107_004843_create_audit_stats::Migration),
             Box::new(m20240213_190221_add_fourfours_stats::Migration),
             Box::new(m20240322_205213_add_content_audit_index::Migration),
@@ -41,6 +42,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20240919_121611_census_subnetwork_index::Migration),
             Box::new(m20241010_151313_audit_stats_performance::Migration),
             Box::new(m20241206_154045_add_beacon_and_state_audit_stats::Migration),
+            Box::new(m20250130_042751_create_audit_transfer_failures::Migration),
         ]
     }
 }
