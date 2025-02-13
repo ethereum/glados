0df9c60b024723202839ae0ac76178c5e9c5f1a5
Jason Carver <ut96caarrs@snkmail.com>
2025-02-13 02:22:33 -0800

    generated entity without sender client info

--- entity/src/audit_internal_failure.rs
+++ entity/src/audit_internal_failure.rs
@@ -8,21 +8,12 @@ pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
     pub audit: i32,
-    pub sender_client_info: i32,
     pub sender_node: i32,
     pub failure_type: i32,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
-    #[sea_orm(
-        belongs_to = "super::client_info::Entity",
-        from = "Column::SenderClientInfo",
-        to = "super::client_info::Column::Id",
-        on_update = "NoAction",
-        on_delete = "NoAction"
-    )]
-    ClientInfo,
     #[sea_orm(
         belongs_to = "super::content_audit::Entity",
         from = "Column::Audit",
@@ -39,12 +30,6 @@ pub enum Relation {
         on_delete = "NoAction"
     )]
     Node,
-}
-
-impl Related<super::client_info::Entity> for Entity {
-    fn to() -> RelationDef {
-        Relation::ClientInfo.def()
-    }
 }
 
 impl Related<super::content_audit::Entity> for Entity {
