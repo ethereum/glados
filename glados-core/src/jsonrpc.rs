849a43b880ff6b6de919f7682566336a9dba80ef
ScottyPoi <scott.simpson@ethereum.org>
2025-03-11 07:08:52 -0600

    fix format error

--- glados-core/src/jsonrpc.rs
+++ glados-core/src/jsonrpc.rs
@@ -150,8 +150,8 @@ impl PortalClient {
     }
 
     pub fn supports_trace(&self) -> bool {
-        self.client_info.contains("trin") 
-            || self.client_info.contains("fluffy") 
+        self.client_info.contains("trin")
+            || self.client_info.contains("fluffy")
             || self.client_info.contains("ultralight")
     }
 }
