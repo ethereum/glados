ab78d53ef1cac84bff0d04819f3de2e3bc56c2a1
ScottyPoi <scott.simpson@ethereum.org>
2025-03-11 06:27:15 -0600

    add ultralight to supports_trace

--- glados-core/src/jsonrpc.rs
+++ glados-core/src/jsonrpc.rs
@@ -150,7 +150,7 @@ impl PortalClient {
     }
 
     pub fn supports_trace(&self) -> bool {
-        self.client_info.contains("trin") || self.client_info.contains("fluffy")
+        self.client_info.contains("trin") || self.client_info.contains("fluffy") || self.client_info.contains("ultralight")
     }
 }
 
