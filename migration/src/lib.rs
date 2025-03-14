0b0884bafd8c87a41a288617120d0e7a5d32579c
Jorge Pérez <jorge@ktlxv.com>
2025-03-14 14:48:25 +0100

    feat: add migrations for blocks and audit_result_latest, and seed for blocks

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,4 +1,8 @@
+use anyhow::{bail, Result};
+use futures::TryStreamExt;
+use sea_orm::DatabaseConnection;
 pub use sea_orm_migration::prelude::*;
+use tracing::{error, info, warn};
 
 mod m20230511_104804_create_node;
 mod m20230511_104811_create_record;
@@ -18,6 +22,8 @@ mod m20240919_121611_census_subnetwork_index;
 mod m20241010_151313_audit_stats_performance;
 mod m20241206_154045_add_beacon_and_state_audit_stats;
 mod m20250130_042751_create_audit_internal_failures;
+mod m20250311_115816_create_blocks;
+mod m20250311_121724_create_audit_result_latest;
 
 pub struct Migrator;
 
@@ -43,6 +49,105 @@ impl MigratorTrait for Migrator {
             Box::new(m20241010_151313_audit_stats_performance::Migration),
             Box::new(m20241206_154045_add_beacon_and_state_audit_stats::Migration),
             Box::new(m20250130_042751_create_audit_internal_failures::Migration),
+            Box::new(m20250311_115816_create_blocks::Migration),
+            Box::new(m20250311_121724_create_audit_result_latest::Migration),
         ]
+    }
+}
+
+pub struct SeedConfig {
+    migration_name: String,
+    url: String,
+    table: String,
+}
+pub trait SeedTrait {
+    fn seeds() -> Vec<SeedConfig>;
+
+    fn seed(
+        conn: &DatabaseConnection,
+        seed_config: SeedConfig,
+    ) -> impl std::future::Future<Output = Result<()>> + Send;
+
+    fn seed_new_migrations(
+        conn: &DatabaseConnection,
+        old_migrations: Vec<String>,
+        skip_seeding: bool,
+    ) -> impl std::future::Future<Output = Result<()>> + Send;
+
+    fn seed_by_table(
+        conn: DatabaseConnection,
+        table: String,
+    ) -> impl std::future::Future<Output = Result<()>> + Send;
+}
+
+impl SeedTrait for Migrator {
+    fn seeds() -> Vec<SeedConfig> {
+        vec![SeedConfig {
+            migration_name: "m20250311_115816_create_blocks".into(),
+            // TODO(ktlxv): Update when file is uploaded
+            url: "http://192.168.2.70:8080/premerge.csv".into(),
+            table: "block".into(),
+        }]
+    }
+
+    async fn seed(conn: &DatabaseConnection, seed_config: SeedConfig) -> Result<()> {
+        info!(
+            "Seeding '{}' table (this might take several minutes)",
+            seed_config.table
+        );
+
+        // Get low level Postgress connection
+        let db = conn.get_postgres_connection_pool();
+
+        // Use streams to avoid saving a large CSV
+        let res = match reqwest::get(&seed_config.url).await {
+            Ok(res) => res,
+            Err(err) => {
+                error!("Error while fetching seed data for table '{}' from {}. Please seed table manually with seed subcommand", seed_config.table, seed_config.url);
+                bail!(err)
+            }
+        };
+
+        let stream = res.bytes_stream().map_err(std::io::Error::other);
+        let tokio_stream = tokio_util::io::StreamReader::new(stream);
+
+        let mut writer = db
+            .copy_in_raw(&(format!("COPY {} FROM stdin csv header", seed_config.table)))
+            .await
+            .unwrap();
+        writer.read_from(tokio_stream).await?;
+
+        if let Err(err) = writer.finish().await {
+            error!("Error seeding {}, please seed manually", seed_config.table);
+            bail!(err)
+        }
+
+        info!("Table {} seeded successfuly", seed_config.table);
+
+        Ok(())
+    }
+
+    async fn seed_new_migrations(
+        conn: &DatabaseConnection,
+        old_migrations: Vec<String>,
+        skip_seeding: bool,
+    ) -> Result<()> {
+        for s in Migrator::seeds() {
+            if !old_migrations.contains(&s.migration_name) {
+                match skip_seeding {
+                    true => warn!("Skipped seeding for migration: {}", &s.migration_name),
+                    false => Migrator::seed(conn, s).await?,
+                }
+            }
+        }
+        Ok(())
+    }
+    async fn seed_by_table(conn: DatabaseConnection, table: String) -> Result<()> {
+        if let Some(seed_config) = Migrator::seeds().into_iter().find(|s| s.table == *table) {
+            Migrator::seed(&conn, seed_config).await?;
+            Ok(())
+        } else {
+            bail!("Seed for table {} not found.", table);
+        }
     }
 }
