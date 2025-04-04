c838e9581b9fdb00eb8894cc36d497b8f75c6d9f
Jason Carver <ut96caarrs@snkmail.com>
2025-04-04 16:52:40 -0700

    fix: update migration to leave node ID in place

--- migration/src/m20250404_202958_internal_failures_replace_node_with_record.rs
+++ migration/src/m20250404_202958_internal_failures_replace_node_with_record.rs
@@ -36,48 +36,10 @@ impl MigrationTrait for Migration {
             "WITH maxsn AS ( select r.*, ROW_NUMBER() OVER (PARTITION BY node_id ORDER BY sequence_number DESC) AS rn FROM record AS r) UPDATE audit_internal_failure AS aif SET sender_record_id=maxsn.id FROM maxsn WHERE aif.sender_node=maxsn.node_id;",
         ).await?;
 
-        // Drop the old sender node column
-        manager
-            .alter_table(
-                Table::alter()
-                    .table(AuditInternalFailure::Table)
-                    .drop_column(AuditInternalFailure::SenderNode)
-                    .to_owned(),
-            )
-            .await?;
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .alter_table(
-                Table::alter()
-                    .table(AuditInternalFailure::Table)
-                    .add_column_if_not_exists(
-                        ColumnDef::new(AuditInternalFailure::SenderNode).integer(),
-                    )
-                    .to_owned(),
-            )
-            .await?;
-        manager
-            .create_foreign_key(
-                ForeignKey::create()
-                    .name("FK_auditinternalfailure_sender_node")
-                    .from(
-                        AuditInternalFailure::Table,
-                        AuditInternalFailure::SenderNode,
-                    )
-                    .to(Node::Table, Node::Id)
-                    .on_update(ForeignKeyAction::Cascade)
-                    .to_owned(),
-            )
-            .await?;
-
-        // For all internal transfer failures, downgrade the data back to use sender node.
-        manager.get_connection().execute_unprepared(
-            "UPDATE audit_internal_failure AS aif SET sender_node=record.node_id FROM record WHERE aif.sender_record_id=record.id;",
-        ).await?;
-
         // Drop the new sender record column
         manager
             .alter_table(
@@ -96,19 +58,11 @@ impl MigrationTrait for Migration {
 enum AuditInternalFailure {
     Table,
     // Foreign key
-    SenderNode,
-    // Foreign key
     SenderRecordId,
 }
 
 #[derive(Iden)]
 enum Record {
-    Table,
-    Id,
-}
-
-#[derive(Iden)]
-enum Node {
     Table,
     Id,
 }
