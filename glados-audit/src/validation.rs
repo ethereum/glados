235c969ea8e3c19a16e962ad7480f3ccb5918dc1
Jason Carver <ut96caarrs@snkmail.com>
2025-05-15 08:20:49 -0700

    chore: upgrade ethportal-api to v0.10.0 (#407)
    * chore: upgrade ethportal-api to v0.10.0
    
    Requires an upgrade of:
    - alloy to 0.15
    - alloy-primitives to 1.1.0
    - eth_trie to 0.6.0
    
    * add EphemeralHeadersFindContent validation logic
    * add EphemeralHeaderOffer validation logic

--- glados-audit/src/validation.rs
+++ glados-audit/src/validation.rs
@@ -118,5 +118,57 @@ fn validate_history(content_key: &HistoryContentKey, content_bytes: &[u8]) -> bo
             warn!("Need to call trusted provider to check receipts correctness.");
             true
         }
+        HistoryContentValue::EphemeralHeadersFindContent(ref found_content) => {
+            if let HistoryContentKey::EphemeralHeadersFindContent(key) = content_key {
+                // Check if any of the header hashes match the requested one
+                if found_content
+                    .headers
+                    .iter()
+                    .any(|h| h.hash_slow() == key.block_hash)
+                {
+                    true
+                } else {
+                    // Log with the headers and the requested hash
+                    warn!(
+                        block_hash = hex_encode(key.block_hash),
+                        headers = ?found_content.headers.iter().map(|h| hex_encode(h.hash_slow())).collect::<Vec<_>>(),
+                        "The returned headers did not include the requested header hash"
+                    );
+                    false
+                }
+            } else {
+                warn!(
+                    content.key = ?content_key,
+                    content.value = ?content,
+                    "Decoding error, wrong key paired with content value"
+                );
+                false
+            }
+        }
+        HistoryContentValue::EphemeralHeaderOffer(ref offered_content) => {
+            if let HistoryContentKey::EphemeralHeaderOffer(key) = content_key {
+                // Check that the offered header matches the key, by hash
+                let offered_hash = offered_content.header.hash_slow();
+                if offered_hash == key.block_hash {
+                    true
+                } else {
+                    // Log with the headers and the requested hash
+                    warn!(
+                        block_hash = hex_encode(key.block_hash),
+                        offered_hash = ?offered_hash,
+                        offered_content = ?offered_content,
+                        "The returned offered content did not include the requested header hash"
+                    );
+                    false
+                }
+            } else {
+                warn!(
+                    content.key = ?content_key,
+                    content.value = ?content,
+                    "Decoding error, wrong key paired with content value"
+                );
+                false
+            }
+        }
     }
 }
